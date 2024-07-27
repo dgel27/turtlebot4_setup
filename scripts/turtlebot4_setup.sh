8300f32adbaf4f60122bb07e2bc28a4c19111d27
dgel27 <dgel27@users.noreply.github.com>
2024-07-27 12:47:45 +0300

    Update turtlebot4_setup.sh
    remove call humble.sh and remove duplicates install

--- scripts/turtlebot4_setup.sh
+++ scripts/turtlebot4_setup.sh
@@ -31,11 +31,16 @@ echo "Setting up Turtlebot4";
 
 sudo apt update && sudo apt upgrade
 
-wget -qO - https://raw.githubusercontent.com/turtlebot/turtlebot4_setup/humble/scripts/humble.sh | bash
-
-sudo apt update && sudo apt upgrade
-
-sudo apt install -y ros-humble-ros-base \
+sudo apt install curl gnupg lsb-release -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt install -y \
+build-essential \
+cmake \
+git \
+wget \
+ros-humble-ros-base \
 ros-humble-turtlebot4-setup \
 ros-humble-turtlebot4-robot \
 ros-humble-irobot-create-control \
